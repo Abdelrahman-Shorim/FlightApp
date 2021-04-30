5a1e3e8a80d4bcce9b702a59e0a17f3168e738bc
Abdelrahman-404 <theotakucuber2002@gmail.com>
2021-04-30 15:20:36 +0200

    abdo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +14,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_popup_clicked()
+{
+    QMessageBox::information(this,"Message","hello",QMessageBox::Ok);
+}
