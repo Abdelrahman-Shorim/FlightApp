184325a31207de7ee7881accf23a7e0954388fd0
nournadrr <nournadrr69@gmail.com>
2021-05-02 20:26:24 +0200

    a

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,4 +14,31 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//    QMessageBox::information(this,"Message","hiiiiiiiiiiiiiiiiiiiiiiiiiiiii",QMessageBox::Ok);
 
+void MainWindow::on_pushButton_2_clicked()
+{
+    QMessageBox::information(this,"Message","hiiiiiiiiiiiiiiiiiiiiiiiiiiiii",QMessageBox::Ok);
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox::information(this,"Message","hiiii",QMessageBox::Ok);
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    QMessageBox::information(this,"Message","hiiiiiiiiiiiiiiiiiiiiiiiiiiiii",QMessageBox::Ok);
+
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    QMessageBox::information(this,"Message","ero",QMessageBox::Ok);
+
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    QMessageBox::information(this,"Message","er",QMessageBox::Ok);
+}
